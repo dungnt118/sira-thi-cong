d4bd6e22b5194cd0cd3e0e94e638af73e7bcf81a
dungnt118 <dungnt118@gmail.com>
2026-04-07 16:44:17 +0700

    feat: implement CI/CD pipeline with reusable Docker deployment workflows and environment configuration management

---
+++ deploy/env_dev.js
@@ -0,0 +1,17 @@
+window.env = {
+    base_url: 'https://api.bac.demego.vn',
+    //oauth login
+    clients: [
+        {
+            client_id: "bac.user",
+            client_secret: "g2RAXgZenebyybWmmfyDWprGhxe5CLAJ",
+            scope: 'openid profile offline_access',
+            title: "Nhân viên"
+        }
+
+    ],
+    //web push
+    PublicVapidKey: "BKi3dJK8IiwbY2QqFZDJ7hOA5Yus7PpkS7kVNH0zvMBQ_h51soB1OLCYu108W7530hXoU8Lp-g8BmkwZUMvar-Y",
+    // Google Maps API Key
+    googleMapsApiKey: ""
+};
